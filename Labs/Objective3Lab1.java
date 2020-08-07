65b0c71d8a91c64a669cb224f5b8568be517630d
Jess Tucker <tuckerjess92@gmail.com>
2020-08-07 15:20:34 -0500

    complete objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 = 20;
+    double num2 = 12;
+    double num3 = 5;
+    double average = (num1 + num2 + num3)/3;
+    System.out.println(average);
+  }
+}
