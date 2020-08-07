76436a9f6211b95d18adf86d52a21e44b8b82fbc
Jess Tucker <tuckerjess92@gmail.com>
2020-08-07 14:25:00 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jess Tucker";
+    int age = 28;
+    String state = "Missouri";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
