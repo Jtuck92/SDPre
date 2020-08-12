fa1599fd3045e85271b51bee5f61240b68c9142b
Jess Tucker <tuckerjess92@gmail.com>
2020-08-12 16:51:53 -0500

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int num = 1;
+    int sum = 0;
+
+     for(num = 1; num <= 20; num++) {
+      sum = sum + num;
+     }
+     System.out.println(sum);
+  }
+}
