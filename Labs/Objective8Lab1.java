53d4c1a9cae129213f60fd48535b45908f2c3af4
Jess Tucker <tuckerjess92@gmail.com>
2020-08-12 16:41:06 -0500

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    int num;
+
+    for(num = 1; num <= 10; num++) {
+      System.out.println(num);
+    }
+    for(num = 10; num >= 1; num--) {
+      System.out.println(num);
+    }
+  }
+}
