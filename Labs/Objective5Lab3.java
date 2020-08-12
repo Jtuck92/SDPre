f9c1e7356f57075d7869f418f6fc0098fd104324
Jess Tucker <tuckerjess92@gmail.com>
2020-08-12 14:57:20 -0500

    complete objective5lab3

--- Labs/Objective5Lab3.java
+++ Labs/Objective5Lab3.java
@@ -9,11 +9,14 @@ public class Objective5Lab3 {
   System.out.println("Please enter a number: ");
   System.out.println(userNum);
 
-  if(userNum >= 0) {
-    System.out.println("The number is positive");
+  if(userNum > 0) {
+    System.out.println("The number greater than zero");
+  }
+  if (userNum == 0) {
+    System.out.println("The numnber is 0");
   }
   else {
-    System.out.println("The number is not positive");
+    System.out.println("The number is less than zero");
     }
   }
 }
